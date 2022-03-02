4578a1034791134530c2501e7d8e3c3c7e260b24
Sharon Zaragoza <sxcz.21.00@gmail.com>
2022-03-02 08:26:56 -0600

    Modelo

---
+++ src/main/java/mx/tecnm/piedad/models/Actores.java
@@ -0,0 +1,19 @@
+package mx.tecnm.piedad.models;
+
+public class Actores {
+	private int id;
+	private String nombre_completo;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre_completo() {
+		return nombre_completo;
+	}
+	public void setNombre_completo(String nombre_completo) {
+		this.nombre_completo = nombre_completo;
+	}
+	
+}
