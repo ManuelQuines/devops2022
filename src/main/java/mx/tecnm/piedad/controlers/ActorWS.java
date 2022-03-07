fb89d65b05e4a7e565464c39573eff65befab73a
Sharon Zaragoza <sxcz.21.00@gmail.com>
2022-03-07 00:21:28 -0600

    ActorWS

--- src/main/java/mx/tecnm/piedad/controlers/ActorWS.java
+++ src/main/java/mx/tecnm/piedad/controlers/ActorWS.java
@@ -1,11 +1,17 @@
 package mx.tecnm.piedad.controlers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,11 +20,11 @@ import org.springframework.web.bind.annotation.RestController;
 import mx.tecnm.piedad.dao.ActorJDBC;
 import mx.tecnm.piedad.models.Actores;
 @RestController
-<<<<<<< HEAD
+
 @RequestMapping("/api/actores")
-=======
-@RequestMapping("/api")
->>>>>>> 9c16af2f7084d6d8178440c903c36196df8296f3
+
+
+
 @CrossOrigin(origins = "*", methods= {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.PATCH})
 
 
@@ -27,7 +33,7 @@ public class ActorWS {
 	@Autowired
 	ActorJDBC repo;
 	
-<<<<<<< HEAD
+
 	@PostMapping()
 	 public ResponseEntity<?> AgregarActor(@RequestBody Actores nuevo_actor){
     	try {
@@ -40,24 +46,34 @@ public class ActorWS {
     	}
     }
 	
+	@GetMapping()
+	public ResponseEntity<?> CatalogoActores(){
+		try {
+			List<Actores> resultado = repo.CatalogoActores();
+			return new ResponseEntity<> (resultado, HttpStatus.OK);	
+		}	catch(DataAccessException e) {
+			System.out.println(e.getMessage());
+			return new ResponseEntity<> (HttpStatus.NO_CONTENT);
+		}	
+	}
 	
 	
-=======
+	@PutMapping("/{actor-id}")
+	public ResponseEntity<?> ModificarActor (@PathVariable ("actor-id") int actorid, @RequestBody Actores actor){
+    	repo.ModificaActor(actor, actorid);
+		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
 	
-			@PostMapping("/actores")
-			 public ResponseEntity<?> AgregarActor(@RequestBody Actores nuevo_actor){
-		    	try {
-		    	repo.AgregarActor(nuevo_actor);
-		    	return new ResponseEntity<>( HttpStatus.CREATED);
-		    } catch (DataAccessException e) {
-		    	System.out.println(e.getMessage());
-		    	return new ResponseEntity<>(HttpStatus.CONFLICT);
-		    	
-		    	}
-		    }
+	@DeleteMapping("/{actor-id}")
+	public ResponseEntity<?> EliminaActor (@PathVariable ("actor-id") int actorid){
+		repo.EliminaActor(actorid);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+
+	
 			
 
->>>>>>> 9c16af2f7084d6d8178440c903c36196df8296f3
 }
 
 			
